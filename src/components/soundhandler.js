5d4eea3db784e57753a8458e0309ade954030f56
rcofie <rebeccacofie@hotmail.co.uk>
2023-03-10 16:34:15 +0000

    Update README, mini code review

--- src/components/soundhandler.js
+++ src/components/soundhandler.js
@@ -2,15 +2,10 @@ AFRAME.registerComponent("soundhandler", {
   init: function () {
     this.soundEl = document.querySelector("[sound]");
     this.marker = document.querySelector("a-marker");
-    this.visible = false;
   },
   tick: function () {
-    if (this.marker.object3D.visible && !this.visible) {
-      this.soundEl.components.sound.stopSound();
+    if (this.marker.object3D.visible) {
       this.soundEl.components.sound.playSound();
-      this.visible = true;
-    } else if (!this.marker.object3D.visible) {
-      this.visible = false;
     }
   },
 });
